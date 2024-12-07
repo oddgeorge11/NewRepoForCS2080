2dcca8b69a506c7c85f64a159bea2c23cac1d727
oddgeorge11 <122923812+oddgeorge11@users.noreply.github.com>
2024-12-06 22:12:46 -0700

    Update assignment.sh with the correct operations now done

--- Assignment7/assignment.sh
+++ Assignment7/assignment.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-apt -y update
-apt install -y nginx
 service nginx start
 sed -i 's/Welcome to nginx!/Welcome to Georges page!/g' /var/www/html/index.nginx-debian.html
 sed -i 's/nginx/nginx (pronounced as EngineX)/g' /var/www/html/index.nginx-debian.html
-
-
+service nginx restart
